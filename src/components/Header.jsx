8f0372bd97fd371111d09bcedf86ae02a0e5d0a5
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2023-04-03 01:10:11 +0200

    ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ export default function Header() {
                             rel="noopener noreferrer"
                             class="flex flex-row flex-wrap justify-center items-center text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                         >
-                            <FaGlobe className="center mr-2" /> WhackBot
+                            <FaDiscord className="center mr-2" /> WhackBot
                         </a>
                         <a
                             role="button"
